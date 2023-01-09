2c055f65cf93f14c47decefc5233f05b0b941083
Saurabh <61317144+html-css-js-art@users.noreply.github.com>
2023-01-09 16:46:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Greymattersbot/url-auto-delete-shortener-bot
+  git clone https://github.com/GreyMatterbots/url-auto-delete-shortener-bot /moviebot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO
+  git clone $UPSTREAM_REPO /moviebot
 fi
+cd /moviebot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
