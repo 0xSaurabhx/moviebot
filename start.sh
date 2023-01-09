efdc8795c4a3bf7d5895c76cfe007467fc04d527
Saurabh <61317144+html-css-js-art@users.noreply.github.com>
2023-01-09 17:27:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
-if [ -z $UPSTREAM_REPO ]
+if [ -z https://github.com/html-css-js-art/moviebot ]
 then
   echo "Cloning main Repository"
   git clone https://github.com/html-css-js-art/moviebot /moviebot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /moviebot
+  git clone https://github.com/html-css-js-art/moviebot /moviebot
 fi
 cd /moviebot
 pip3 install -U -r requirements.txt
